c732892c89573481c413ca7853ab257a9976f586
Christian C. Romero <id.32@outlook.com>
2018-03-28 20:01:39 +0200

    game graphics

--- sketch.js
+++ sketch.js
@@ -1,8 +1,55 @@
 'use strict';
 
-const dim = 10
-const col = 40
-const row = 40
+class G2048 {
+  constructor(wid, hei) {
+    this.wid = wid
+    this.hei = hei
+    this.matx = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    this.game = true
+    this.colr = [
+      '#cdc0b4','#eee4da','#ede0c8','#f2b179','#f59563','#f67c5f',
+      '#f65e3b','#edcf72','#edcc61','#edc850','#edc53f','#edc22e'
+    ]
+    this.spawn()
+    this.spawn()
+  }
+
+  spawn() {
+    let x, y
+    do {
+      x = Math.floor(Math.random() * 4)
+      y = Math.floor(Math.random() * 4)
+    } while (this.matx[x][y] !== 0) 
+    this.matx[x][y] = 1
+  }
+
+  draw() {
+    let mar = 15
+    let wid = (this.wid - mar) / 4 - mar
+    let hei = (this.hei - mar) / 4 - mar
+    noStroke()
+    textSize(55)
+    textFont('Arial')
+    textStyle(BOLD)
+    textAlign(CENTER, CENTER)
+    translate(15, 15)
+    fill('#bbada0')
+    rect(0, 0, 500, 500, 5)
+    for (let y = 0; y < 4; y++) {
+      for (let x = 0; x < 4; x++) {
+        let val = this.matx[x][y]
+        let xoff = x * (wid + mar) + mar
+        let yoff = y * (hei + mar) + mar
+        fill(this.colr[val])
+        rect(xoff, yoff, hei, wid, 2)
+        fill('#776e65')
+        if (val > 0) text(Math.pow(2, val), xoff + wid/2, yoff + hei/2)
+      }
+    }
+  }
+}
+
+const g2048 = new G2048(500, 500)
 
 let cnv
 
@@ -13,10 +60,11 @@ function centerCanvas() {
 }
 
 function setup() {
-  cnv = createCanvas(col * dim + 1, row * dim + 1)
+  cnv = createCanvas(530, 530)
   centerCanvas()
-  background(255, 0, 200)
+  background('#faf8ef')
   frameRate(10)
+  g2048.draw()
 }
 
 function windowResized() {
